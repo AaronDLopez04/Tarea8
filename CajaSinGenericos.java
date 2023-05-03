27a6a2712ef847a03efceb109c507199cec5cde7
AaronDLopez04 <126019490+AaronDLopez04@users.noreply.github.com>
2023-05-03 14:12:49 -0600

    Add files via upload

---
+++ CajaSinGenericos.java
@@ -0,0 +1,25 @@
+package org example;
+
+public class CajaSinGenericos {
+    Object[] content;
+    
+    public CajaSinGenericos() {
+    }
+    
+    public CajaSinGenericos(Object[] content) {
+        this.content = content;
+    }
+    public String toString(){
+        return "CajaSinGenericos{" +
+        "content=" + Arrays.toString(content) +
+        '}';
+    }
+    
+    public void add (int pos, T objeto){
+        content[pos] = objeto;
+    }
+    public T get(int pos){
+        return content[pos];
+    }
+    
+}
